bef77a4d002d5fa63cda3003d45b079e645cbd42
Nikita Chernyshov <chernyllexs@mail.ru>
2021-12-23 19:49:27 +0300

    added a few things

---
+++ src/main/java/com/chernyllexs/thymeleafe/models/SearchPerson.java
@@ -0,0 +1,34 @@
+package com.chernyllexs.thymeleafe.models;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+public class SearchPerson {
+    @NotEmpty(message = "Should not be empty")
+    @Size(message = "Surname should be between 2 and 20 symbols")
+    private String surname;
+    @NotEmpty(message = "Should not be empty")
+    @Size(message = "Name should be between 2 and 20 symbols")
+    private String name;
+
+    public SearchPerson(@NotEmpty(message = "Should not be empty") @Size(message = "Surname should be between 2 and 20 symbols") String surname, @NotEmpty(message = "Should not be empty") @Size(message = "Name should be between 2 and 20 symbols") String name) {
+        this.surname = surname;
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
